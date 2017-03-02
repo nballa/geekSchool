08d5a41b3c639546331f75c88b226c22a4cd05b3
Natalia <vinterhaxa@admins-mbp.home>
2017-03-02 21:55:09 +0100

    Initial Commit

---
+++ Lesson1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson1
+//
+//  Created by Natalia on 02.03.17.
+//  Copyright © 2017 Natalia Balla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson1
+//
+//  Created by Natalia on 02.03.17.
+//  Copyright © 2017 Natalia Balla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
